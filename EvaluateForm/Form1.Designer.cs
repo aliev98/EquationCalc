d680daa923255c123c7be7de4f9ac46f0a1ece28
Ali Elias <kuba-98@hotmail.com>
2021-01-17 23:17:40 +0100

    first commit

---
+++ EvaluateForm/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace EvaluateForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.variable_add = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtFormula = new System.Windows.Forms.TextBox();
+            this.lblFormula = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // variable_add
+            // 
+            this.variable_add.BackColor = System.Drawing.SystemColors.Control;
+            this.variable_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.variable_add.Location = new System.Drawing.Point(377, 136);
+            this.variable_add.Name = "variable_add";
+            this.variable_add.Size = new System.Drawing.Size(152, 50);
+            this.variable_add.TabIndex = 0;
+            this.variable_add.Text = "Add a variable";
+            this.variable_add.UseVisualStyleBackColor = false;
+            this.variable_add.Click += new System.EventHandler(this.variable_add_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(377, 192);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(152, 44);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResult.Location = new System.Drawing.Point(377, 44);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(142, 31);
+            this.txtResult.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(416, 13);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(56, 18);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result";
+            // 
+            // txtFormula
+            // 
+            this.txtFormula.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFormula.Location = new System.Drawing.Point(12, 44);
+            this.txtFormula.Multiline = true;
+            this.txtFormula.Name = "txtFormula";
+            this.txtFormula.Size = new System.Drawing.Size(316, 31);
+            this.txtFormula.TabIndex = 4;
+            // 
+            // lblFormula
+            // 
+            this.lblFormula.AutoSize = true;
+            this.lblFormula.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFormula.Location = new System.Drawing.Point(123, 13);
+            this.lblFormula.Name = "lblFormula";
+            this.lblFormula.Size = new System.Drawing.Size(77, 20);
+            this.lblFormula.TabIndex = 5;
+            this.lblFormula.Text = "Formula";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(541, 248);
+            this.Controls.Add(this.lblFormula);
+            this.Controls.Add(this.txtFormula);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.variable_add);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Equation calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button variable_add;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtFormula;
+        private System.Windows.Forms.Label lblFormula;
+    }
+}
+
---
+++ EvaluateForm/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.Threading;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace EvaluateForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int y = 88;
+        List<Control> textboxlist = new List<Control>();
+        //List<textboxandlabel> textboxlist2 = new List<textboxandlabel>();
+        string name1 = "";
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+            Font myfont = new Font("Microsoft sant serif", 11.0f, FontStyle.Bold);
+            txtFormula.Font = myfont;
+            txtResult.Font = myfont;
+            txtResult.ReadOnly = true;
+            txtResult.BackColor = Color.White;
+
+            btnCalculate.FlatStyle = FlatStyle.Flat;
+            btnCalculate.BackColor = Color.Transparent;
+            btnCalculate.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnCalculate.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnCalculate.FlatAppearance.BorderColor = Color.FromName("Control");
+
+            variable_add.FlatStyle = FlatStyle.Flat;
+            variable_add.BackColor = Color.Transparent;
+            variable_add.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            variable_add.FlatAppearance.MouseOverBackColor = Color.Gray;
+            variable_add.FlatAppearance.BorderColor = Color.FromName("Control");
+        }
+
+        Control GetControlByName (string Name)
+        {
+            foreach (Control c in this.Controls)
+                if (c.Name == Name)
+                    return c;
+
+            return null;
+        }
+
+        Control latestTextbox;
+        private void variable_add_Click (object sender, EventArgs e)
+        {
+
+            name1 = Microsoft.VisualBasic.Interaction.InputBox ("Vad ska variabeln heta?", "Title", "", 170, 70);
+
+            //var latestTextbox = GetControlByName (this.name1); // får fram den nyaste textboxen
+
+            //foreach (var item in textboxlist)
+            //{
+            //    if (item.name == name1)
+            //    {
+            //        messagebox.show("this variable already exists", "", messageboxbuttons.ok, messageboxicon.error);
+
+            //        int lastindex = textboxlist.count() - 1;
+
+            //        return;
+            //    }
+
+            //    if (item.name.tolower() == name1.tolower())
+            //    {
+            //        messagebox.show("this variable already exists", "", messageboxbuttons.ok, messageboxicon.error);
+            //        return;
+            //    }
+            //}
+
+            if (name1.Length == 0)
+            {
+                MessageBox.Show("You didn't enter variable name");
+                return;
+            }
+
+            if (double.TryParse (name1, out double output))
+            {
+                MessageBox.Show ("Variable names can't be numbers");
+                return;
+            }
+
+            for (int i = 0; i < textboxlist.Count(); i++)
+            {
+                if (name1 == textboxlist[i].Name)
+                {
+                    MessageBox.Show("This variable already excists");
+                    return;
+                }
+            }
+
+            Controls.Add(
+            new TextBox()
+            {
+                Location = new Point(70, y),
+
+                Size = new Size(56, 22),
+
+                Name = $"{name1}" // viktigt
+            }
+            );
+
+           latestTextbox = GetControlByName(this.name1);
+           textboxlist.Add (latestTextbox); // lägger till den nya textboxen i listan
+
+            Controls.Add(
+                new Label()
+                {
+                    Location = new System.Drawing.Point(16, y),
+                    Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0))),
+                    Text = $"{name1}",
+                    Size = new System.Drawing.Size(52, 17),
+                }
+                );
+            
+            // new stuff
+            Controls.Add(
+                  new Label()
+                  {
+                      Location = new System.Drawing.Point(130, y),
+                      Font = new System.Drawing.Font("Microsoft Sans Serif", 9.0F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0))),
+                      Text = $"X",
+                      Name = $"{name1}label",
+                      ForeColor = Color.Red,
+                      Size = new System.Drawing.Size(52, 17),
+                  }
+                  ) ;
+
+            var latestlabel = GetControlByName($"{name1}label");
+            labellist.Add(latestlabel);
+            //latestlabel.Click += delegate { helloworld(name1, latestlabel.Name); };
+          
+            //new stuff
+
+            y = y + 30;
+            
+            this.Height += 30;
+        }
+
+        List<Control> labellist = new List<Control>() { };
+        void btnPrint_Click (object sender, EventArgs e)
+        {
+            var latesttextbox = GetControlByName($"{name1}");
+            latestTextbox.Dispose();
+        }
+
+        private void removevar (string textname, string labelname)
+        {
+            bool booltext = false;
+
+            var righttextbox = GetControlByName(textname);
+
+            if (righttextbox != null) booltext = true;
+
+            bool boollabel = false;
+
+            var rightlabel = GetControlByName(labelname);
+
+            if (rightlabel != null) boollabel = true;
+
+            if (booltext && boollabel)
+            {
+                righttextbox.Dispose();
+            }
+        }
+
+        object ConvertToAny (string input)
+        {
+            int i;
+
+            if (int.TryParse(input, out i))
+                return i;
+
+            double d;
+
+            if (double.TryParse(input, out d))
+                return d;
+  
+            return input;
+        }
+
+        private void btnCalculate_Click (object sender, EventArgs e)
+        {
+            var list1 = textboxlist;
+
+            string parameterlist = "";
+
+            foreach (var item in list1)
+            {
+
+                parameterlist += $"double {item.Name.ToLower()}";
+
+                if (list1.IndexOf(item) != list1.Count - 1)
+                {
+                    parameterlist += ",";
+                }
+            }
+
+            string s = txtFormula.Text;
+
+            string[] words = s.Split('^');
+
+            double result = 0;
+
+            if (words.Count() > 1)
+            {
+                result = Math.Pow (double.Parse(words[0]), double.Parse(words[1]));
+            }
+
+
+
+            //char[] charray = s.ToCharArray();
+
+            foreach (var item in s)
+            {
+                char j = item;
+
+                if (s.IndexOf(j) == s.Length - 1)
+                {
+                    if (!double.TryParse(j.ToString(), out double output))
+                    {
+                         break;
+                    }
+
+                   if (item != '0')
+                   {
+                        //txtFormula.Text += '.';
+                        //txtFormula.Text += '0';
+                   }
+                }
+            }
+
+            var replace = result.ToString();
+            
+            for (int i = 0; i < words.Count(); i++)
+            {
+
+            }
+
+            // Turn the equation into a function.
+            string function_text = (result == 0) ?
+            "public static class Evaluator" +
+            "{" +                               //  (double x, double y)
+            "    public static double Evaluate ( " + parameterlist + " )" +
+            "    {" +          // 1 + x
+            "        return " + txtFormula.Text + ";" +
+            "    }" +
+            "}"
+            :
+            "public static class Evaluator" +
+            "{" +                               //  (double x, double y)
+            "    public static double Evaluate ( " + parameterlist + " )" +
+            "    {" +          // 1 + x
+            "        return " + result + ";" +
+            "    }" +
+            "}";
+
+
+            // Compile the function.
+
+            CodeDomProvider code_provider = CodeDomProvider.CreateProvider("C#");
+
+           // Generate a non-executable assembly in memory.
+            CompilerParameters parameters = new CompilerParameters();
+            parameters.GenerateInMemory = true;
+            parameters.GenerateExecutable = false;
+    
+
+            // Compile the code.
+            CompilerResults results = code_provider.CompileAssemblyFromSource (parameters, function_text);
+            
+                // If there are errors, display them.
+                if (results.Errors.Count > 0)
+                {
+                    string msg = "Error compiling the expression.";
+
+                    foreach (CompilerError compiler_error in results.Errors)
+                    {
+                         msg += "\n" + compiler_error.ErrorText;
+                    }
+
+                    MessageBox.Show (msg, "Expression Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                else
+                {
+                    // Get the Evaluator class type.
+                    Type evaluator_type = results.CompiledAssembly.GetType("Evaluator");
+
+                    // Get a MethodInfo object describing the Evaluate method.
+                    MethodInfo method_info = evaluator_type.GetMethod("Evaluate");
+                    // Make the parameter list.
+
+                    //object[] method_params = new object[]
+                    //{
+                    //    double.Parse (txtX.Text),
+                    //    double.Parse (txtY.Text),
+                    //};
+
+                    //  lista med argument som skickas som parametrar, som sedan görs om till en array
+                    List <object> method_paramslist = new List<object>() { };
+
+                    foreach (var item in list1)
+                    {
+                        if (item.Text.Length == 0)
+                        {
+                            item.Text = "0";
+                        }
+
+                        object element = 0;
+                        Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
+
+                        if (double.TryParse(item.Text, out double output))
+                        {
+                            //element = double.Parse (item.Text);
+                            element = output;
+                        }
+
+                        else
+                        {
+
+                        }
+
+                        method_paramslist.Add(element);
+                    }
+
+                object[] method_paramsarray = method_paramslist.ToArray();
+
+                // Execute the method.
+                double expression_result = (double) method_info.Invoke(null, method_paramsarray);
+
+                // Display the returned result.
+                txtResult.Text = expression_result.ToString();
+            }
+        }
+    }
+}
